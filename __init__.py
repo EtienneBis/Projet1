d3a92462ba5a1fe1cdd235c5b0817c0a7901b287
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2024-09-02 17:55:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template_string, render_template, jsonify
-
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
 app = Flask(__name__)
 
 # @app.route('/')
