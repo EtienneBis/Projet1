efde1ca4c29f7879cff94e702a7801786fe3af64
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2024-09-02 18:02:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,17 @@
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return render_template('hello.html')
+@app.route('/')
+def index():
+    return render_template('hello.html')
 
-# @app.route('/signin')
-# def signin():
-#     return render_template('signin.html')
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
-# @app.route('/signup')
-# def signup():
-#     return render_template('signup.html')
-
-@app.route('/', methods=['GET'])
-def return_home():
-    if 'authentifie' in session and session['authentifie']:
-        return render_template('hello.html')
-    else:
-        return redirect(url_for('authentification'))
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
