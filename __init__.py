6a24f647a120689f3ac997ac4e1113e8a1945449
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2024-09-02 16:52:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template_string, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def index():
     return render_template('index.html')
 
