e88a8883e1b4e17fe548457af567a4e9f5d00b62
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2024-09-05 19:21:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -80,7 +80,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
 # Route pour le profil
 @app.route('/profile')
