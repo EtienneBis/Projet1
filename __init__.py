b478567b93c804fd3ddab1644b4220f9a1db7035
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2024-09-05 18:30:52 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -55,7 +55,7 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        hashed_password = generate_password_hash(password, method='sha256')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
